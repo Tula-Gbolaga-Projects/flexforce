19901a11e1baf376d1e0c2aaf1c383d05407789c
Tula-Gbolaga <oguntulagbolaga@gmail.com>
2024-07-30 22:14:53 +0100

    initial dashboard build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { JobSeekerDashboard } from "./pages/JobSeeker";
+import { AgencyDashboard } from "./pages/Agency";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<JobSeekerDashboard />} />
+      <Route path="/Agency" element={<AgencyDashboard />} />
 
       {/* <Route path="/profile" element={<Profile />} /> */}
     </Routes>
