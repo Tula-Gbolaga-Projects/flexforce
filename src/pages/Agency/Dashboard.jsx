2bb9c6d45ae71470c7269d94bec0dc40fe55bb00
Tula-Gbolaga-Fusion <gbolaga@reachcinema.io>
2024-08-09 23:52:41 +0100

    Job seeker dashboard

--- src/pages/Agency/Dashboard.jsx
+++ src/pages/Agency/Dashboard.jsx
@@ -6,7 +6,7 @@ const AgencyDashboard = () => {
       <div className="m-4 rounded bg-gray-100 p-3">
         <h1 className="text-black text-[20px]">Overview</h1>
         <div>
-          <OverviewItem count={5} title={"Jobs that have not been assinged"} />
+          <OverviewItem count={5} title={"Jobs that have not been matched"} />
           <OverviewItem count={23} title={"Applications that are pending"} />
           <OverviewItem count={113} title={"Shifts scheduled for this week"} />
           <OverviewItem count={15} title={"Shifts that are ongoing"} />
--- src/pages/JobSeeker/Dashboard.jsx
+++ src/pages/JobSeeker/Dashboard.jsx
@@ -1,9 +1,18 @@
-import { PortalWrapper } from "../../components";
+import { PortalWrapper, OverviewItem } from "../../components";
 
 const JobSeekerDashboard = () => {
   return (
     <PortalWrapper>
-      <h1 className="text-black">Job Seeker Dashboard</h1>
+      <div className="m-4 rounded bg-gray-100 p-3">
+        <h1 className="text-black text-[20px]">Overview</h1>
+        <div>
+          <OverviewItem count={1} title={"Jobs you have been invited for "} />
+          <OverviewItem count={8} title={"Applications you sent"} />
+          <OverviewItem count={5} title={"Shifts scheduled for this week"} />
+          <OverviewItem count={1} title={"Shifts in progress"} />
+          <OverviewItem count={5} title={"Agency Invitations"} />
+        </div>
+      </div>
     </PortalWrapper>
   );
 };
