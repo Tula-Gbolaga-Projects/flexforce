aaa521a48ec9a54bc61c469abe4d87f6f9b46556
Tula-Gbolaga-Fusion <gbolaga@reachcinema.io>
2024-08-12 06:30:54 +0100

    test on github actions

--- src/pages/JobSeeker/Dashboard.jsx
+++ src/pages/JobSeeker/Dashboard.jsx
@@ -4,7 +4,7 @@ const JobSeekerDashboard = () => {
   return (
     <PortalWrapper>
       <div className="m-4 rounded bg-gray-100 p-3">
-        <h1 className="text-black text-[20px]">Overviewww</h1>
+        <h1 className="text-black text-[20px]">Overview</h1>
         <div>
           <OverviewItem count={1} title={"Jobs you have been invited for "} />
           <OverviewItem count={8} title={"Applications you sent"} />
